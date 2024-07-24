c8df5cda73a0bec78228252b16854d998ebcf59b
Mohd Asad~ <mohdasad024@gmail.com>
2024-07-24 16:17:05 +0530

    first commit

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./styles.css";
-function Header(){
-    return <div className="header"></div>
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        CryptoTracker<span style={{ color: "var(--blue)" }}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/compare">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/watchlist">
+          <p className="link">Watchlist</p>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
