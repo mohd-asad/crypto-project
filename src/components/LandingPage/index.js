f0b28d222e1ae3ee92d0f1cb35ba922e6ca4369b
Mohd Asad~ <mohdasad024@gmail.com>
2024-08-09 19:56:16 +0530

    landing page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,27 +1,65 @@
 import React from "react";
 import "./styles.css";
 import Button from "../Common/Button";
-import phone from "../../assets/phone 1.png"
-import gradient from "../../assets/gradient 1.png"
+import phone from "../../assets/phone 1.png";
+import gradient from "../../assets/gradient 1.png";
+import { motion } from "framer-motion";
 
 function MainComponent() {
   return (
     <div className="main-flex">
       <div className="left-component">
-        <h1 className="track-crypto"> Track Crypto</h1>
-        <h1 className="real-time"> Real Time.</h1>
-        <p className="text">
+        <motion.h1
+          className="track-crypto"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {" "}
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="real-time"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          {" "}
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+        >
           {" "}
           Track crypto through a public api in real time. Visit the dashboard to
           do so!
-        </p>
-        <div className="btn-flex">
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.5 }}
+        >
           <Button text={"Dashboard"}></Button>
           <Button text={"Share"} outLined={true}></Button>
-        </div>
+        </motion.div>
       </div>
       <div className="phone-component">
-        <img src={phone} className="phone"></img>
+        <motion.img
+          src={phone}
+          className="phone"
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 2,
+            repeat: Infinity,
+          }}
+        ></motion.img>
         <img src={gradient} className="gradient"></img>
       </div>
     </div>
